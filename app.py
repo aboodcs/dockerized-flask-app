e323af893ae791a79eede90a73e51c5417613a47
aboodcs <aboodyahya411@gmail.com>
2026-06-08 02:56:29 +0300

    Add Docker Compose configuration

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+
+# connect to redis service from docker-compose
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 @app.route("/")
 def home():
     return "Hello GitHub!"
+
+@app.route("/visit")
+def visit():
+    count = r.incr("visit_count")
+    return f"Visit count: {count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
